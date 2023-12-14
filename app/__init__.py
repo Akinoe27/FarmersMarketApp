ed0fbb3182e1a3aeb714608714935cc6317ab7aa
Akinoe27 <140145591+Akinoe27@users.noreply.github.com>
2023-12-14 16:55:37 +0800

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,28 @@
+#call the flask object
 from flask import Flask
+from flask_mysqldb import MySQL
 
+#create a new name for Flask
 app = Flask(__name__)
 
+
+#database credentials XAMPP
+#hostname "localhost"
+app.config['MYSQL_HOST'] = 'localhost'
+#username "root"
+app.config['MYSQL_USER'] = 'root'
+#password "root"
+app.config['MYSQL_PASSWORD'] = ''
+#database
+app.config['MYSQL_DB'] = 'marvin'
+
+#initialize all XAMPP credentials
+mysql = MySQL (app)
+
+
+
+
+
+#call routes from the flask
 from app import routes
+
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,32 @@
 from app import app
+from app import mysql
 from flask import render_template
+from flask import jsonify
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/products')
+def products():
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM product") 
+    product_data = cursor.fetchall()
+    cursor.close()
+    return render_template('products.html', products = product_data)
+
+@app.route('/orders')
+def showOrders():
+    return render_template('order_confirmation.html')
+
+#route to test connection
+# @app.route('/testconnect')
+# def test_db():
+#     try:
+#         cursor = mysql.connection.cursor()
+#         cursor.close()
+#         print("Database credentials are all correct")
+#         return jsonify({'message':'Database credentials are all goods'})
+#     except Exception as e: 
+#         print("Some of the database credentials is incorrect",str(e))
+#         return jsonify({'message':'Database credentials error'})
